a2dcbf946baa74f0c9d8d9050928b59b278f326e
杨思倩 <siqian20150501@163.com>
2018-09-12 08:49:31 +0800

    重写Django的认证后端，同时支持用户名和手机号登录

--- meiduo_mall/meiduo_mall/apps/users/utils.py
+++ meiduo_mall/meiduo_mall/apps/users/utils.py
@@ -1,3 +1,10 @@
+import re
+
+from django.contrib.auth.backends import ModelBackend
+
+from .models import User
+
+
 def jwt_response_payload_handler(token, user=None, request=None):
     """
     自定义jwt认证成功返回数据
@@ -7,3 +14,34 @@ def jwt_response_payload_handler(token, user=None, request=None):
         'user_id': user.id,
         'username': user.username
     }
+
+
+def get_user_by_account(account):
+    """
+    根据账号查询用户对象
+    :param account: 账号：username or mobile
+    :return: none or User
+    """
+    try:
+        # 账号为手机号
+        if re.match('^1[3-9]\d{9}$',account):
+            user = User.objects.get(mobile=account)
+        else:
+            user = User.objects.get(username=account)
+    except User.DoesNotExist:
+        return None
+    else:
+        return user
+
+
+
+
+
+class UsernameMobileAuthBackend(ModelBackend):
+    """自定义用户名和密码认证"""
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        # 根据账号查询用户对象  账号：用户名或手机号
+        user = get_user_by_account(username)
+        # 将密码传给check_password验证
+        if user is not None and user.check_password(password):
+            return user
--- meiduo_mall/meiduo_mall/settings/dev.py
+++ meiduo_mall/meiduo_mall/settings/dev.py
@@ -231,3 +231,7 @@ CORS_ORIGIN_WHITELIST = (
     'api.meiduo.site:8000',
 )
 CORS_ALLOW_CREDENTIALS = True # 允许携带cookie
+
+AUTHENTICATION_BACKENDS = [
+    'users.utils.UsernameMobileAuthBackend'
+]
