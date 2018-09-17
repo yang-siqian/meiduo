402cf905adce90f415b0d16bfaf08e47e3d062c3
杨思倩 <siqian20150501@163.com>
2018-09-17 20:09:19 +0800

    收货地址功能实现

---
+++ meiduo_mall/meiduo_mall/apps/oauth/views.py
@@ -0,0 +1,104 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework import status
+from rest_framework.generics import GenericAPIView
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework_jwt.settings import api_settings
+
+from oauth.exception import QQAPIError
+from oauth.models import OAuthQQUser
+from oauth.serializers import OAuthQQUserSerializer
+from oauth.utils import OAuthQQ
+
+
+class QQAuthURLView(APIView):
+    """
+    获取QQ登录的url
+    """
+    def get(self, request):
+        """
+        提供用于qq登录的url
+        """
+        state = request.query_params.get('state')
+        # 如果前端没有传state就返回首页
+        if not state:
+            state = '/'
+        oauth_qq = OAuthQQ(state=state)
+        login_url = oauth_qq.get_auth_url()
+        # 返回链接地址
+        return Response({'oauth_url': login_url})
+
+
+class QQAuthUserView(GenericAPIView):
+    """
+    QQ登录的用户
+    """
+    serializer_class = OAuthQQUserSerializer
+
+    def get(self, request):
+        """
+        获取qq登录的用户数据
+        """
+        # 提取code参数
+        code = request.query_params.get('code')
+        if not code:
+            return Response({'message': '缺少code'}, status=status.HTTP_400_BAD_REQUEST)
+
+        oauth = OAuthQQ()
+
+        try:
+            # 凭借code向QQ服务器发起请求，获取access_token
+            access_token = oauth.get_access_token(code)
+            # 凭借access_token向QQ服务器发起请求，获取用户openid
+            openid = oauth.get_openid(access_token)
+        except QQAPIError:
+            return Response({'message': 'QQ服务异常'}, status=status.HTTP_503_SERVICE_UNAVAILABLE)
+
+        # 判断用户是否存在
+        try:
+            # 凭借openid查询此用户之前是否在美多中绑定过账户
+            qq_user = OAuthQQUser.objects.get(openid=openid)
+        except OAuthQQUser.DoesNotExist:
+            # 如果未绑定，用户第一次使用QQ登录，手动创建绑定身份的access_token
+            access_token = OAuthQQUser.generate_save_user_token(openid)
+            return Response({'access_token': access_token})
+        else:
+            # 如果已经绑定，找到用户, 生成登录凭证JWT token，并返回
+            user = qq_user.user
+            jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
+            jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
+
+            payload = jwt_payload_handler(user)
+            token = jwt_encode_handler(payload)
+
+            response = Response({
+                'token': token,
+                'user_id': user.id,
+                'username': user.username
+            })
+            return response
+
+    def post(self, request):
+        """
+        保存QQ登录用户数据
+        """
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+
+        # 生成已登录的token
+        jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
+        jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
+
+        payload = jwt_payload_handler(user)
+        token = jwt_encode_handler(payload)
+
+        response = Response({
+            'token': token,
+            'user_id': user.id,
+            'username': user.username
+        })
+
+        return response
