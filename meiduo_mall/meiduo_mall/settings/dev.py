d01b05702fa93a313ddb5cf2700587a1bebe8163
杨思倩 <siqian20150501@163.com>
2018-09-20 19:05:59 +0800

    docker配置

--- meiduo_mall/meiduo_mall/settings/dev.py
+++ meiduo_mall/meiduo_mall/settings/dev.py
@@ -45,10 +45,15 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
+    'ckeditor',  # 富文本编辑器
+    'ckeditor_uploader',  # 富文本编辑器上传图片模块
+    'django_crontab',  # 定时任务
     'users.apps.UsersConfig',
     'verifications.apps.VerificationsConfig',
     'oauth.apps.OauthConfig',
     'areas.apps.AreasConfig',
+    'goods.apps.GoodsConfig',
+    'contents.apps.ContentsConfig',
 ]
 
 MIDDLEWARE = [
@@ -67,7 +72,7 @@ ROOT_URLCONF = 'meiduo_mall.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -267,3 +272,34 @@ REST_FRAMEWORK_EXTENSIONS = {
     # 缓存存储
     'DEFAULT_USE_CACHE': 'default',
 }
+
+
+# django文件存储
+DEFAULT_FILE_STORAGE = 'meiduo_mall.utils.fastdfs.fdfs_storage.FastDFSStorage'
+
+# FastDFS
+FDFS_URL = 'http://10.211.55.4:8888/'
+FDFS_CLIENT_CONF = os.path.join(BASE_DIR, 'utils/fastdfs/client.conf')
+
+
+# 富文本编辑器ckeditor配置
+CKEDITOR_CONFIGS = {
+    'default': {
+        'toolbar': 'full',  # 工具条功能
+        'height': 300,  # 编辑器高度
+        # 'width': 300,  # 编辑器宽
+    },
+}
+CKEDITOR_UPLOAD_PATH = ''  # 上传图片保存路径，使用了FastDFS，所以此处设为''
+
+# 生成的静态html文件保存目录
+GENERATED_STATIC_HTML_FILES_DIR = os.path.join(os.path.dirname(os.path.dirname(BASE_DIR)), 'front_end_pc')
+
+# 定时任务
+CRONJOBS = [
+    # 每5分钟执行一次生成主页静态文件
+    ('*/5 * * * *', 'contents.crons.generate_static_index_html', '>> /Users/yangsiqian/Desktop/django_md/meiduo/meiduo_mall/logs/crontab.log')
+]
+
+# 解决crontab中文问题
+CRONTAB_COMMAND_PREFIX = 'LANG=zh_cn.UTF-8'
---
+++ meiduo_mall/meiduo_mall/utils/fastdfs/fdfs_storage.py
@@ -0,0 +1,39 @@
+from django.conf import settings
+from django.core.files.storage import Storage
+from django.utils.deconstruct import deconstructible
+from fdfs_client.client import Fdfs_client
+
+@deconstructible
+class FasfDFSStorage(Storage):
+    """自定义文件存储系统"""
+    def __init__(self, base_url=None, client_conf=None):
+        if base_url is None:
+            base_url = settings.FDFS_URL
+        self.base_url = base_url
+
+        if client_conf is None:
+            client_conf = settings.FDFS_CLIENT_CONF
+        self.client_conf = client_conf
+
+    def _open(self, name, mode='rb'):
+        pass
+
+    def _save(self, name, content):
+        """
+        在FastDFS中保存文件
+        :param name: 传入的文件名
+        :param content: 文件内容
+        :return: 保存到数据库中的FastDFS的文件名
+        """
+        client = Fdfs_client(self.client_conf)
+        ret = client.upload_by_buffer(content.read())
+        if ret.get('Status') != "Upload successed.":
+            raise Exception('upload file failed')
+        file_name = ret.get('Remote file_id')
+        return file_name
+
+    def exist(self, name):
+        return False
+
+    def url(self, name):
+        return self.base_url + name
