5b89423d0ccef0b1546c3a9dc4dc9db9b09eea01
杨思倩 <siqian20150501@163.com>
2018-09-22 08:54:45 +0800

    商品详情页静态化

--- meiduo_mall/celery_tasks/main.py
+++ meiduo_mall/celery_tasks/main.py
@@ -10,7 +10,7 @@ celery_app = Celery('meiduo')
 celery_app.config_from_object('celery_tasks.config')
 
 # 自动注册celery任务
-celery_app.autodiscover_tasks(['celery_tasks.sms', 'celery_tasks.email'])
+celery_app.autodiscover_tasks(['celery_tasks.sms', 'celery_tasks.email', 'celery_tasks.html'])
 
 # 开启celery的命令
 # celery -A celery_tasks.main worker -l info
